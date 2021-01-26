6f575ff1550f6d51c636afbb600ad412b8911065
Moctader <54657677+Moctader@users.noreply.github.com>
2021-01-26 09:29:32 +0200

    Add files via upload

---
+++ public_html/js/choices.js
@@ -0,0 +1,58 @@
+/* 
+    Created on : May, 2019 
+    Author     : Liisa Auer, Oulu University of Applied Sciences
+    Licence    : CC-BY-4.0
+*/
+
+/*
+ * Student name :
+ * Started date :
+ * Ended   date :
+ * 
+ */
+
+function v01() {
+
+}
+
+function v02() {
+
+}
+
+function v03() {
+    const NEG = "One of the numbers is negative."
+    const POS = "Both numbers are either positive or negative."
+    
+}
+
+function v04() {
+    const REC = "rectangle";
+    const SQ = "square";
+
+}
+
+function v05() {
+    const EVEN = "even";
+    const ODD = "odd";
+
+}
+
+function v06() {
+
+}
+
+function v07() {
+
+}
+
+function v08() {
+
+}
+
+function v09() {
+	const FLOWER1 = '<img alt="flower1" src="img/flower1.png">';
+	const FLOWER2 = '<img alt="flower2" src="img/flower2.png">';
+	const FLOWER3 = '<img alt="flower3" src="img/flower3.png">';
+	const FLOWER4 = '<img alt="flower4" src="img/flower4.png">';  
+
+}
