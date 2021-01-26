da8dfd5bdb68ea4b17c99966a20082875d0761d1
Moctader <moctader.golam@gmail.com>
2021-01-26 14:57:36 +0200

    THREE DONE

--- public_html/js/choices.js
+++ public_html/js/choices.js
@@ -12,16 +12,52 @@
  */
 
 function v01() {
+    var greeting;
+    let birthYear = Number(document.getElementById("year").value)
+    let today = new Date(); // current date
+    let current_year = today.getFullYear();
+    let age = current_year - birthYear;
+
+    if (age >= 18){
+        document.getElementById("allow").innerHTML="Welcome!"
+    } else {
+        document.getElementById("allow").innerHTML="too young"
+    }
+
+    
 
 }
 
 function v02() {
+    let number1 = Number(document.getElementById("number1").value)
+    let number2 = Number(document.getElementById("number2").value)
 
+    if (number1 < number2){
+        document.getElementById("smaller").innerHTML= number1
+
+    } else {
+        document.getElementById("smaller").innerHTML= number2
+    }
 }
+
 
 function v03() {
     const NEG = "One of the numbers is negative."
     const POS = "Both numbers are either positive or negative."
+
+    let number1 = Number(document.getElementById("number1").value)
+    let number2 = Number(document.getElementById("number2").value)
+
+    let mul =  number1 * number2;
+
+    if (mul > 0) {
+        document.getElementById("negative").innerHTML= POS
+
+    } else {
+        document.getElementById("negative").innerHTML= NEG
+
+    }
+
     
 }
 
